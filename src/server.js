028b7858e586499ab234f0490dbeda8c802a5b1d
Kevin Van den Abeele <info@beeles-place.be>
2015-08-12 12:50:37 +0200

    Added caching for images (all content types containing image) Cleaned up some code and added docs App should not correctly boot on single and dual core machines!

--- src/server.js
+++ src/server.js
@@ -8,21 +8,33 @@ var logger = require("./logging/logger").makeLogger("SERVER");
 /**
  * This function will start the server.
  *
- * @param route The function that will handle the routing of the requests.
+ * @param id The server id.
  * @param handles An array with handles which can be matched by specific routes.
  */
 function start(id, handles) {
-    function onRequest(request, response) {
-        var pathName = url.parse(request.url).pathname;
-
-        logger.INFO("---------------------------------------------------------");
-        logger.INFO("SID: " + id + " -> Request for " + pathName + " received.");
-
-        router.route(handles, pathName, request, response);
-    }
-
-    http.createServer(onRequest).listen(8080, "0.0.0.0");
+    http.createServer(makeRequestHandler(id, handles)).listen(8080, "0.0.0.0");
     logger.INFO("Server started!");
+}
+
+/**
+ * Creates and returns the request handler closure function.
+ *
+ * @param id The server id.
+ * @param handles An array with handles which can be matched by specific routes.
+ * @returns {Function} The actual request handler.
+ */
+function makeRequestHandler(id, handles) {
+    /**
+     * Request handler.
+     *
+     * @param request The request object.
+     * @param response The response object.
+     */
+    return function onRequest(request, response) {
+        var pathName = url.parse(request.url).pathname;
+        logger.INFO("IP: " + request.connection.remoteAddress + " \tassigned to server: " + id + "\t-> requested: " + pathName);
+        router.route(handles, pathName, request, response);
+    };
 }
 
 exports.start = start;
