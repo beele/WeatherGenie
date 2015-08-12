c655a5bc025add20c1c2c6979fa1055f239b1490
Kevin Van den Abeele <info@beeles-place.be>
2015-08-12 16:55:28 +0200

    SVG files now in the HTML source without the need for some javascript magic to convert them! (looking at you safari!)

--- www/js/utils.js
+++ www/js/utils.js
@@ -11,36 +11,6 @@ var lon = null;
 var styleSheetSelection = null;
 
 /**
- * Converts all img tags with the class svg to real svg tags.
- * The main advantage of this is that we can style the svgs.
- */
-function convertIMGtoSVG() {
-    $('img.svg').each(function(){
-        var $img = $(this);
-        var imgID = $img.attr('id');
-        var imgClass = $img.attr('class');
-        var imgURL = $img.attr('src');
-
-        $.get(imgURL, function(data) {
-            // Get the SVG tag, ignore the rest
-            var $svg = $(data).find('svg');
-            // Add replaced image's ID to the new SVG
-            if(typeof imgID !== 'undefined') {
-                $svg = $svg.attr('id', imgID);
-            }
-            // Add replaced image's classes to the new SVG
-            if(typeof imgClass !== 'undefined') {
-                $svg = $svg.attr('class', imgClass+' replaced-svg');
-            }
-            // Remove any invalid XML tags as per http://validator.w3.org
-            $svg = $svg.removeAttr('xmlns:a');
-            // Replace image with new SVG
-            $img.replaceWith($svg);
-        }, 'xml');
-    });
-}
-
-/**
  * Determines the user's GEO location.
  */
 function determineGeoLocation() {
